e8c8b65f4c20053c06393f041d8b933470611ff5
RicardoBeny <126669644+RicardoBeny@users.noreply.github.com>
2023-11-16 14:46:43 +0000

    Post Vcard

--- server-api/app/Http/Controllers/api/VcardController.php
+++ server-api/app/Http/Controllers/api/VcardController.php
@@ -6,10 +6,17 @@ use App\Models\Vcard;
 use App\Http\Resources\VcardResource;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreUpdateVcardRequest;
 
 class VcardController extends Controller
 {
     public function index(){
         return VcardResource::collection(Vcard::all());  
+    }
+
+    public function store(StoreUpdateVcardRequest $request)
+    {
+        $newVcard = Vcard::create($request->validated());
+        return new VcardResource($newVcard);
     }
 }
--- server-api/routes/api.php
+++ server-api/routes/api.php
@@ -32,6 +32,7 @@ Route::get('users', [UserController::class, 'index']);
 Route::get('vcards', [VcardController::class, 'index']);
 Route::get('vcards/{vcard}/transactions', [TransactionController::class, 'getTransactionOfVcard']);
 Route::get('vcards/{vcard}/categories', [CategoryController::class, 'getCategoryOfVcard']);
+Route::post('vcards', [VcardController::class, 'store']);
 
 //Transactions
 
