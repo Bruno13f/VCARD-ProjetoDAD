91a5bdfa09b0099a723edfcbaaa98dc6f5c4f21a
RicardoBeny <126669644+RicardoBeny@users.noreply.github.com>
2023-11-16 13:51:49 +0000

    Get e Put

--- server-api/app/Http/Requests/StoreUpdateVcardRequest.php
+++ server-api/app/Http/Requests/StoreUpdateVcardRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreUpdateTaskRequest extends FormRequest
+class StoreUpdateVcardRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
