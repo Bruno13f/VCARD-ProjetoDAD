77f51a4c7e318ad153b969bfe5b2af8eb14a2acc
RicardoBeny <126669644+RicardoBeny@users.noreply.github.com>
2023-11-16 11:34:25 +0000

    TransactionController

--- server-api/app/Http/Controllers/api/TransactionController.php
+++ server-api/app/Http/Controllers/api/TransactionController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Models\Vcard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    //
+    public function getTransactionOfVcard(Vcard $vcard){
+        return $vcard->transactions;
+    }
 }
