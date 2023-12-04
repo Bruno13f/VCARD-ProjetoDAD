6fa0f46b681337612cd0724a6beb6467867b5258
RicardoBeny <126669644+RicardoBeny@users.noreply.github.com>
2023-12-04 20:49:04 +0000

    Transactions Table com category

--- app_client/src/components/transactions/TransactionTable.vue
+++ app_client/src/components/transactions/TransactionTable.vue
@@ -1,69 +1,73 @@
 <script setup>
 
-  const props = defineProps({
-    transactions: {
-      type: Array,
-      default: () => [],
-    },
-    showId: {
-      type: Boolean,
-      default: true,
-    },
-    showVcard: {
-      type: Boolean,
-      default: true,
-    },
-    showDateTime: {
-      type: Boolean,
-      default: true,
-    },
-    showType: {
-      type: Boolean,
-      default: true,
-    },
-    showValue: {
-      type: Boolean,
-      default: true,
-    },
-    showOldBalance: {
-      type: Boolean,
-      default: true,
-    },
-    showNewBalance: {
-      type: Boolean,
-      default: true,
-    },
-    showPaymentType: {
-      type: Boolean,
-      default: true,
-    },
-    showDescription: {
-      type: Boolean,
-      default: false,
-    },
-    showPaymentReference: {
-      type: Boolean,
-      default: true,
-    },
-    showEditButton: {
-      type: Boolean,
-      default: true,
-    },
-    showDeleteButton: {
-      type: Boolean,
-      default: true,
-    }
-  })
-
-  const emit = defineEmits(['edit', 'delete'])
-
-  const editClick = (transaction) => {
-      emit('edit', transaction)
+const props = defineProps({
+  transactions: {
+    type: Array,
+    default: () => [],
+  },
+  showId: {
+    type: Boolean,
+    default: true,
+  },
+  showVcard: {
+    type: Boolean,
+    default: true,
+  },
+  showDateTime: {
+    type: Boolean,
+    default: true,
+  },
+  showType: {
+    type: Boolean,
+    default: true,
+  },
+  showValue: {
+    type: Boolean,
+    default: true,
+  },
+  showOldBalance: {
+    type: Boolean,
+    default: true,
+  },
+  showNewBalance: {
+    type: Boolean,
+    default: true,
+  },
+  showPaymentType: {
+    type: Boolean,
+    default: true,
+  },
+  showDescription: {
+    type: Boolean,
+    default: false,
+  },
+  showPaymentReference: {
+    type: Boolean,
+    default: true,
+  },
+  showEditButton: {
+    type: Boolean,
+    default: true,
+  },
+  showCategory: {
+    type: Boolean,
+    default: true,
+  },
+  showDeleteButton: {
+    type: Boolean,
+    default: true,
   }
+})
 
-  const deleteClick = (transaction) => {
-      emit('delete', transaction)
-  }
+const emit = defineEmits(['edit', 'delete'])
+
+const editClick = (transaction) => {
+  emit('edit', transaction)
+}
+
+const deleteClick = (transaction) => {
+  emit('delete', transaction)
+}
 
 </script>
 
@@ -80,43 +84,36 @@
         <th v-if="showNewBalance">New Balance</th>
         <th v-if="showPaymentType">Payment Type</th>
         <th v-if="showPaymentReference">Payment Reference</th>
+        <th v-if="showCategory">Category</th>
         <th v-if="showDescription">Description</th>
         <th v-if="showEditButton || showDeleteButton"></th>
       </tr>
     </thead>
     <tbody>
-      <tr
-        v-for="transaction in transactions"
-        :key="transaction.id"
-      >
+      <tr v-for="transaction in transactions" :key="transaction.id">
         <td v-if="showId">{{ transaction.id }}</td>
-        <td>{{ transaction.vcard.phone_number}}</td>
+        <td>{{ transaction.vcard.phone_number }}</td>
         <td v-if="showDateTime">{{ transaction.datetime }}</td>
-        <td v-if="showType">{{ transaction.type == 'C' ? 'Credit' : 'Debit'}}</td>
+        <td v-if="showType">{{ transaction.type == 'C' ? 'Credit' : 'Debit' }}</td>
         <td v-if="showValue" :class="transaction.type == 'C' ? 'text-success' : 'text-danger'">
           {{ transaction.type == 'C' ? '+ ' + transaction.value : '- ' + transaction.value }}</td>
-        <td v-if="showOldBalance">{{ transaction.old_balance + ' €'}}</td>
-        <td v-if="showNewBalance">{{ transaction.new_balance + ' €'}}</td>
+        <td v-if="showOldBalance">{{ transaction.old_balance + ' €' }}</td>
+        <td v-if="showNewBalance">{{ transaction.new_balance + ' €' }}</td>
         <td v-if="showPaymentType">{{ transaction.payment_type }}</td>
         <td v-if="showPaymentReference">{{ transaction.payment_reference }}</td>
+        <td v-if="showCategory">
+          {{ transaction.category_id ? transaction.category_id.name : 'No Category' }}
+        </td>
+
         <td v-if="showDescription">{{ transaction.description }}</td>
-        <td
-          class="text-end"
-          v-if="showEditButton || showDeleteButton"
-        >
+        <td class="text-end" v-if="showEditButton || showDeleteButton">
           <div class="d-flex justify-content-end">
-            <button
-              class="btn btn-xs btn-light"
-              @click="editClick(transaction)"
-              v-if="showEditButton"
-            ><i class="bi bi-xs bi-pencil"></i>
+            <button class="btn btn-xs btn-light" @click="editClick(transaction)" v-if="showEditButton"><i
+                class="bi bi-xs bi-pencil"></i>
             </button>
 
-            <button
-              class="btn btn-xs btn-light"
-              @click="deleteClick(transaction)"
-              v-if="showDeleteButton"
-            ><i class="bi bi-xs bi-x-square-fill"></i>
+            <button class="btn btn-xs btn-light" @click="deleteClick(transaction)" v-if="showDeleteButton"><i
+                class="bi bi-xs bi-x-square-fill"></i>
             </button>
           </div>
         </td>
